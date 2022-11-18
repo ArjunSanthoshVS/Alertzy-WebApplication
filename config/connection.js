9b6ad685994a68b97a5f36e0ad1de3bd8198db67
Arjun Santhosh <arjunsanthosh738@gmail.com>
2022-11-18 10:03:55 +0530

    atlas url update

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
     db: null
 }
 module.exports.connect = function (done) {
-    const url = 'mongodb+srv://Arjun:UtKC2pI08DgNkLbb@cluster0.9jrdgji.mongodb.net/?retryWrites=true&w=majority'
+    const url = process.env.URL
     const dbname = 'Alertzy'
     mongoClient.connect(url, (err, data) => {
         if (err) return done(err)

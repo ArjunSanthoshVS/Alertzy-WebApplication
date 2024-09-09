efaf202314466c735001686bc94437261cff56a2
Arjun Santhosh <arjunsanthosh738@gmail.com>
2024-09-09 21:58:22 +0530

    Updated Aletrzy

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,37 @@
-const mongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb');
+require('dotenv').config(); // Load the environment variables
+
 const state = {
     db: null
-}
+};
+
 module.exports.connect = function (done) {
-    const url = process.env.URL
-    const dbname = 'Alertzy'
-    mongoClient.connect(url, (err, data) => {
-        if (err) return done(err)
-        state.db = data.db(dbname)
-    })
-    done()
-}
+    console.log(done,"doneee.....");
+    
+    const url = process.env.URL; // Load URL from environment variable
+    console.log(url,"djnskjgkjns");
+    
+    const dbname = 'Alertzy';
+
+    MongoClient.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        tls: true,  // Force using TLS/SSL
+        tlsAllowInvalidCertificates: false, // Don't allow invalid certificates
+        tlsInsecure: false // Ensure secure connection
+    }, (err, client) => {
+        if (err) {
+            console.error('Connection Failed', err);
+            return done(err); // Handle connection error
+        }
+    
+        state.db = client.db(dbname); // Set the connected database instance
+        console.log('Database connected successfully');
+        done(); // Call done once the connection is established
+    });
+    
+};
+
 module.exports.get = function () {
-    return state.db
-}
+    return state.db;
+};
